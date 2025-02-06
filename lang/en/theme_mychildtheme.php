30dee892d181c0e9f9c38f3bd3b7213893f9d4b4
Emily <emily-l93@hotmail.com>
2025-02-06 14:02:46 +0200

    Create theme_mychildtheme.php

---
+++ lang/en/theme_mychildtheme.php
@@ -0,0 +1,279 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language file.
+ *
+ * @package   theme_moove
+ * @copyright 2017 Willian Mano - http://conecti.me
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'MyChildTheme';
+$string['configtitle'] = 'My Child Theme';
+$string['choosereadme'] = 'My Child Theme is based on the Moove theme and is a modern, highly customizable Moodle theme. Use it directly or as a base for further customization.';
+
+$string['currentinparentheses'] = '(current)';
+$string['region-side-pre'] = 'Right';
+$string['login'] = 'Welcome to the Virtual Learning Environment (VLE).';
+$string['prev_section'] = 'Previous section';
+$string['next_section'] = 'Next section';
+$string['themedevelopedby'] = 'This theme was developed by';
+$string['search_site'] = 'Search on site';
+$string['search_forums'] = 'Search on course forums';
+$string['platform_access'] = 'Access to the platform';
+$string['readmore'] = 'Read More';
+$string['stayintouch'] = 'Stay in touch';
+$string['madewitmoodle'] = 'Proudly made with';
+$string['madeby'] = 'Made with';
+$string['by'] = 'by';
+$string['discipline_progress'] = 'Topic\'s progress';
+$string['access'] = 'Access';
+$string['prev_activity'] = 'Previous activity';
+$string['next_activity'] = 'Next activity';
+$string['donthaveanaccount'] = 'Don\'t have an account?';
+$string['signinwith'] = 'Sign in with';
+
+// General settings tab.
+$string['generalsettings'] = 'General';
+$string['logo'] = 'Logo';
+$string['logodesc'] = 'The logo is displayed in the header.';
+$string['favicon'] = 'Custom favicon';
+$string['favicondesc'] = 'Upload your own favicon.  It should be an .ico file.';
+$string['preset'] = 'Theme preset';
+$string['preset_desc'] = 'Pick a preset to broadly change the look of the theme.';
+$string['presetfiles'] = 'Additional theme preset files';
+$string['presetfiles_desc'] = 'Preset files can be used to dramatically alter the appearance of the theme. See <a href="https://docs.moodle.org/dev/Boost_Presets">Boost presets</a> for information on creating and sharing your own preset files.';
+$string['loginbgimg'] = 'Login page background';
+$string['loginbgimg_desc'] = 'Upload your custom background image for the login page.';
+$string['brandcolor'] = 'Brand colour';
+$string['brandcolor_desc'] = 'The accent colour.';
+$string['navbarheadercolor'] = 'Navbar header color';
+$string['navbarheadercolor_desc'] = 'The top navbar header color';
+$string['secondarymenucolor'] = 'Secondary menu color';
+$string['secondarymenucolor_desc'] = 'Secondary menu background color';
+$string['navbarbg'] = 'Navbar color';
+$string['navbarbg_desc'] = 'The left navbar color';
+$string['navbarbghover'] = 'Navbar hover color';
+$string['navbarbghover_desc'] = 'The left navbar hover color';
+$string['coursepresentation'] = 'Course presentation';
+$string['coursepresentationdesc'] = '<b>Default presentation:</b> The default course appearence.<br><b>Cover image:</b> Will be showed a cover image on the top of the course page(the first image in course summary files will be used).';
+$string['coursedefault'] = 'Default presentation';
+$string['coursecover'] = 'Cover image';
+$string['courselistview'] = 'Enable course list view';
+$string['courselistviewdesc'] = 'Switch the courses view from box to list view';
+$string['fontsite'] = 'Site font';
+$string['fontsite_desc'] = 'Default font site. You can try out the fonts on <a href="https://fonts.google.com">Google Fonts site</a>.';
+$string['enablecourseindex'] = 'Enable course index';
+$string['enablecourseindex_desc'] = 'You can show/hide course index navigation';
+
+// Advanced settings tab.
+$string['advancedsettings'] = 'Advanced';
+$string['rawscsspre'] = 'Raw initial SCSS';
+$string['rawscsspre_desc'] = 'In this field you can provide initialising SCSS code, it will be injected before everything else. Most of the time you will use this setting to define variables.';
+$string['rawscss'] = 'Raw SCSS';
+$string['rawscss_desc'] = 'Use this field to provide SCSS or CSS code which will be injected at the end of the style sheet.';
+$string['googleanalytics'] = 'Google Analytics V4 Code';
+$string['googleanalyticsdesc'] = 'Please enter your Google Analytics V4 code to enable analytics on your website. The code format shold be like [G-XXXXXXXXXX]';
+
+// Frontpage settings tab.
+$string['frontpagesettings'] = 'Frontpage';
+$string['displaymarketingboxes'] = 'Show front page marketing boxes';
+$string['displaymarketingboxesdesc'] = 'If you want to see the boxes, select yes <strong>then click SAVE</strong> to load the input fields.';
+$string['marketingsectionheading'] = 'Marketing section heading title';
+$string['marketingsectioncontent'] = 'Marketing section content';
+$string['marketingicon'] = 'Marketing Icon {$a}';
+$string['marketingheading'] = 'Marketing Heading {$a}';
+$string['marketingcontent'] = 'Marketing Content {$a}';
+
+$string['disableteacherspic'] = 'Disable teachers picture';
+$string['disableteacherspicdesc'] = 'This setting hides the teachers\' pictures from the course cards.';
+
+$string['sliderfrontpageloggedin'] = 'Show slideshow in frontpage after login?';
+$string['sliderfrontpageloggedindesc'] = 'If enabled, the slideshow will be showed in the frontpage page replacing the header image.';
+$string['slidercount'] = 'Slider count';
+$string['slidercountdesc'] = 'Select how many slides you want to add <strong>then click SAVE</strong> to load the input fields.';
+$string['sliderimage'] = 'Slider picture';
+$string['sliderimagedesc'] = 'Add an image for your slide. Recommended size is 1500px x 540px or higher.';
+$string['slidertitle'] = 'Slide title';
+$string['slidertitledesc'] = 'Add the slide\'s title.';
+$string['slidercaption'] = 'Slider caption';
+$string['slidercaptiondesc'] = 'Add a caption for your slide';
+
+$string['numbersfrontpage'] = 'Show site numberss';
+$string['numbersfrontpagedesc'] = 'If enabled, display the number of active users and courses in the frontpage.';
+$string['numbersfrontpagecontent'] = 'Numbers section content';
+$string['numbersfrontpagecontentdesc'] = 'You can add any text to the left side of the numbers section';
+$string['numbersfrontpagecontentdefault'] = '<h2>Trusted by 25,000+ happy customers.</h2>
+                    <p>With lots of unique blocks, you can easily build <br class="d-none d-sm-block d-md-none d-xl-block">
+                        a page without coding. Build your next website <br class="d-none d-sm-block d-md-none d-xl-block">
+                        within few minutes.</p>';
+$string['numbersusers'] = 'Active users accessing our amazing resources';
+$string['numberscourses'] = 'Courses made for you that you can trust!';
+
+$string['faq'] = 'FAQ';
+$string['faqcount'] = 'FAQ questions';
+$string['faqcountdesc'] = 'Select how many questions you want to add <strong>then click SAVE</strong> to load the input fields.<br>If you don\'t want a FAQ, just select 0.';
+$string['faqquestion'] = 'FAQ question {$a}';
+$string['faqanswer'] = 'FAQ answer {$a}';
+
+// Footer settings tab.
+$string['footersettings'] = 'Footer';
+$string['website'] = 'Website URL';
+$string['websitedesc'] = 'Main company Website';
+$string['mobile'] = 'Mobile';
+$string['mobiledesc'] = 'Enter Mobile No. Ex: +5598912341234';
+$string['mail'] = 'E-Mail';
+$string['maildesc'] = 'Company support e-mail';
+$string['facebook'] = 'Facebook URL';
+$string['facebookdesc'] = 'Enter the URL of your Facebook. (i.e http://www.facebook.com/myinstitution)';
+$string['x'] = 'X URL';
+$string['xdesc'] = 'Enter the URL of your X (formerly Twitter). (i.e http://www.x.com/myinstitution)';
+$string['tiktok'] = 'TikTok URL';
+$string['tiktokdesc'] = 'Enter the URL of your TikTok profile (e.g., https://www.tiktok.com/@myinstitution).';
+$string['linkedin'] = 'Linkedin URL';
+$string['linkedindesc'] = 'Enter the URL of your Linkedin. (i.e http://www.linkedin.com/myinstitution)';
+$string['youtube'] = 'Youtube URL';
+$string['youtubedesc'] = 'Enter the URL of your Youtube. (i.e https://www.youtube.com/user/myinstitution)';
+$string['instagram'] = 'Instagram URL';
+$string['instagramdesc'] = 'Enter the URL of your Instagram. (i.e https://www.instagram.com/myinstitution)';
+$string['topfooterimg'] = 'Footer image';
+$string['topfooterimgdesc'] = 'Upload your custom footer image here if you want to replace the default image. Recommended size is 1500px x 400px or higher.';
+$string['disablebottomfooter'] = 'Disable bottom footer';
+$string['disablebottomfooterdesc'] = 'Disables the orange bottom footer';
+
+$string['contactus'] = 'Contact us';
+$string['followus'] = 'Follow us';
+
+// custom
+
+$string['helpcenter'] = 'Help Center';
+$string['ael'] = 'Academic English Lab';
+
+#footer
+$string['footershortwelcomemessage'] = 'Welcome to the {$a} VLE.';
+$string['links'] = 'Links';
+$string['contact'] = 'Contact';
+$string['allrightsreserved'] = 'All rights reserved';
+$string['termsofuse'] = 'Terms of use';
+$string['termsofusemsg'] = 'This system contains information that is the property of UNICAF LTD and is for authorized use only. Unauthorized access is prohibited. Activities of users on this system are monitored. Anyone using this system expressly consents to such monitoring and is advised that if such monitoring reveals possible evidence of criminal activity, evidence of such activity may be provided to law enforcement officials.';
+$string['address'] = 'Address';
+$string['telephone'] = 'Telephone';
+$string['email'] = 'Email';
+$string['mainwebsite'] = 'Main Website';
+$string['studentpanel'] = 'Student Panel';
+
+#settings
+$string['univinfosettings'] = 'University Info';
+$string['univname'] = 'University Name';
+$string['univname_desc'] = 'University Name will be included in the footer';
+
+$string['univmainwebsite'] = 'Main Website URL';
+$string['univmainwebsite_desc'] = '(Optional) The URL of the main website';
+
+$string['univstudentpanel'] = 'Student Panel';
+$string['univstudentpanel_desc'] = '(Optional) The URL of the Student Panel';
+
+$string['univaddress'] = 'Address';
+$string['univaddress_desc'] = '(Optional) Physical address of the university';
+
+$string['univtelephone'] = 'Telephone';
+$string['univtelephone_desc'] = '(Optional) Phone number';
+
+$string['univemail'] = 'Email';
+$string['univemail_desc'] = 'The email address of the university';
+
+$string['loginhelpstringname'] = 'Having trouble logging in?';
+
+$string['loginhelpmsg'] = '
+<p><h5>How can you access the VLE</h5></p>
+<p>Access to the Virtual Learning Environment (VLE) is granted to whomever has been registered to any module at least once. <br>
+To register you need to access your Student Panel (SIS), the link is under Links section below.</p>
+<p><h5>Credentials</b></h5>
+<p><b>Username:</b> It is your personal email address you used to register with us. Unless you’ve
+requested to change your email address in our records then, use the new email address as
+username.<br />
+<b>Password:</b> It is the password you received via email when you registered for a module. In case
+you change that password, the latest password you entered is the valid one.</p>
+
+<p>
+<h5>Login Instructions</h5>
+    <ol>
+        <li>Type in your email address in the ​ “Username”​ input field.</li>
+        <li>Type in you correct password in the​ “Password”​ input field.</li>
+        <li>Then click on ​ “Log in”​ button.</li>
+    </ol>
+</p>
+
+<p>If you forgot your ​<b>VLE</b>​ password, you can reset it by using the ​“Forgotten your username or password?”​ link located 
+below.</p>';
+
+
+
+// Forum.
+$string['forumsettings'] = 'Forum';
+$string['forumsettingsdesc'] = 'Configure the general forum settings for the theme here.';
+$string['forumhtmlemailheader'] = 'Email header';
+$string['forumhtmlemailheaderdesc'] = 'Configure the email header for the forum messages.';
+$string['forumhtmlemailfooter'] = 'Email footer';
+$string['forumhtmlemailfooterdesc'] = 'Configure the email footer for the forum messages.';
+$string['forumcustomtemplate'] = 'Use a pretty template for forum mails';
+$string['forumcustomtemplatedesc'] = 'Enable that in order to use a pretty template when sending forum messages via email. If not enabled standard formatting of forum mails will be used.';
+
+$string['certificates'] = 'Certificates';
+$string['certificatestitle'] = 'My certificates';
+$string['subtitleallcertificates'] = 'Certificates from all courses';
+$string['subtitlecoursecertificates'] = 'Course certificates';
+$string['nohavecertificates'] = 'You no have certificates yet';
+$string['coursesections'] = 'Course sections';
+
+// Mypublic page.
+$string['aboutme'] = 'About me';
+$string['personalinformation'] = 'Personal information';
+$string['addcontact'] = 'Add contact';
+$string['removecontact'] = 'Remove contact';
+
+// Theme settings.
+$string['themesettings:accessibility'] = 'Accessibility';
+$string['themesettings:fonttype'] = 'Font type';
+$string['themesettings:defaultfont'] = 'Default font';
+$string['themesettings:dyslexicfont'] = 'Dyslexic font';
+$string['themesettings:enableaccessibilitytoolbar'] = 'Enable accessibility toolbar';
+$string['themesettingg:successfullysaved'] = 'Accessibility settings successfully saved';
+
+// Accessibility features.
+$string['accessibility:fontsize'] = 'Font size';
+$string['accessibility:decreasefont'] = 'Decrease font size';
+$string['accessibility:resetfont'] = 'Reset font size';
+$string['accessibility:increasefont'] = 'Increase font size';
+$string['accessibility:sitecolor'] = 'Site color';
+$string['accessibility:resetsitecolor'] = 'Reset site color';
+$string['accessibility:sitecolor2'] = 'Low contrast 1';
+$string['accessibility:sitecolor3'] = 'Low contrast 2';
+$string['accessibility:sitecolor4'] = 'High contrast';
+
+// Data privacy.
+$string['privacy:metadata:preference:accessibilitystyles_fontsizeclass'] = 'The user\'s preference for font size.';
+$string['privacy:metadata:preference:accessibilitystyles_sitecolorclass'] = 'The user\'s preference for site color.';
+$string['privacy:metadata:preference:thememoovesettings_fonttype'] = 'The user\'s preference for font type.';
+$string['privacy:metadata:preference:thememoovesettings_enableaccessibilitytoolbar'] = 'The user\'s preference for enable the accessibility toolbar.';
+
+$string['privacy:accessibilitystyles_fontsizeclass'] = 'The current preference for the font size is: {$a}.';
+$string['privacy:accessibilitystyles_sitecolorclass'] = 'The current preference for the site color is: {$a}.';
+$string['privacy:thememoovesettings_fonttype'] = 'The current preference for the font type is: {$a}.';
+$string['privacy:thememoovesettings_enableaccessibilitytoolbar'] = 'The current preference for enable accessibility toolbar is to show it.';
