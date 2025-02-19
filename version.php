c5fc2a1b4779bf064cbffb8c70fef61c8d73bb43
Emily <emily-l93@hotmail.com>
2025-02-19 09:11:33 +0200

    Update version.php

--- version.php
+++ version.php
@@ -30,7 +30,7 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'theme_mychildtheme';
 
 // This is the version of the plugin.
-$plugin->version = 2025011502;
+$plugin->version = 2025011505;
 
 // This is the named version.
 $plugin->release = '1.0.0';
